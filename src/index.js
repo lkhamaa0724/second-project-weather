691374840f667c3e6063529e6ea28ea2f46a5e24
lkhamaa0724 <75447127+lkhamaa0724@users.noreply.github.com>
2023-04-18 21:10:12 -0400

    updated html
    updated html

--- src/index.js
+++ src/index.js
@@ -18,20 +18,142 @@ function formatDate(date) {
 		"Saturday",
 	];
 	let day = days[dayIndex];
-	return `${days[dayIndex]} ${hours}:${minutes}`;
+	return `${days[dayIndex]}, ${hours}:${minutes}`;
 }
+
+function emojiDisplay(temp) {
+	if (temp < 0) {
+		return "😒";
+	} else if (temp > 15) {
+		return "😊";
+	} else if (temp < 15) {
+		return "🙂";
+	} else if (temp > 20) {
+		return "😀";
+	} else if (temp > 25) {
+		return "😄";
+	} else {
+		return "😏";
+	}
+}
+
 function displayWeatherCondition(response) {
-	console.log(response);
+	// console.log(response);
 	document.querySelector("#city").innerHTML = response.data.name;
 	document.querySelector("#temperature").innerHTML =
 		Math.round(response.data.main.temp) + "°C";
+
+	document.querySelector(".mon > .temp").innerHTML =
+		Math.round(response.data.main.temp) + "°C";
+
+	document.querySelector(".mon > .emoji").innerHTML = emojiDisplay(
+		Math.round(response.data.main.temp)
+	);
+
 	document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 	document.querySelector("#wind").innerHTML = Math.round(
 		response.data.wind.speed
 	);
 	document.querySelector("#description").innerHTML =
-		response.data.weather[0].main;
+		response.data.weather[0].main +
+		"<img src='https://openweathermap.org/img/wn/" +
+		response.data.weather[0].icon +
+		"@2x.png'>";
 }
+
+function displayForecast(myData) {
+	console.log(myData.data.list);
+	// Example forecast data for one week
+	const forecastData = myData.data.list;
+
+	// Group forecast data by date
+	const groupedData = forecastData.reduce((result, data) => {
+		// Convert timestamp to a date string in YYYY-MM-DD format
+		const dateStr = new Date(data.dt_txt).toISOString().slice(0, 10);
+
+		// If date hasn't been added to result yet, add it with initial high/low values
+		if (!result[dateStr]) {
+			result[dateStr] = {
+				high: data.main.temp_max,
+				low: data.main.temp_min,
+			};
+		} else {
+			// Otherwise, update high/low values if necessary
+			if (data.main.temp_max > result[dateStr].high) {
+				result[dateStr].high = data.main.temp_max;
+			}
+			if (data.main.temp_min < result[dateStr].low) {
+				result[dateStr].low = data.main.temp_min;
+			}
+		}
+
+		return result;
+	}, {});
+
+	console.log(groupedData);
+	const firstDate = Object.keys(groupedData)[0];
+	const firstData = groupedData[firstDate];
+	const first = firstData.high;
+	const firstLow = firstData.low;
+	const tueTemp = document.querySelector(".tue > .temp");
+	const tueEmoji = document.querySelector(".tue > .emoji");
+	tueTemp.innerHTML =
+		Math.round(first) + "°C" + "<br/>" + Math.round(firstLow) + "°C";
+	tueEmoji.innerHTML = emojiDisplay(Math.round(first));
+
+	const secondDate = Object.keys(groupedData)[1];
+	const secondData = groupedData[secondDate];
+	const second = secondData.high;
+	const secondLow = secondData.low;
+	const wedTemp = document.querySelector(".wed > .temp");
+	const wedEmoji = document.querySelector(".wed > .emoji");
+	wedTemp.innerHTML =
+		Math.round(second) + "°C" + "<br/>" + Math.round(secondLow) + "°C";
+	wedEmoji.innerHTML = emojiDisplay(Math.round(second));
+
+	const thirdDate = Object.keys(groupedData)[2];
+	const thirdData = groupedData[thirdDate];
+	const third = thirdData.high;
+	const thirdLow = thirdData.low;
+	const thuTemp = document.querySelector(".thu > .temp");
+	const thuEmoji = document.querySelector(".thu > .emoji");
+	thuTemp.innerHTML =
+		Math.round(third) + "°C" + "<br/>" + Math.round(thirdLow) + "°C";
+	thuEmoji.innerHTML = emojiDisplay(Math.round(third));
+
+	const fourthDate = Object.keys(groupedData)[3];
+	const fourthData = groupedData[fourthDate];
+	const fourth = fourthData.high;
+	const fourthLow = fourthData.low;
+	const friTemp = document.querySelector(".fri > .temp");
+	const friEmoji = document.querySelector(".fri > .emoji");
+	friTemp.innerHTML =
+		Math.round(fourth) + "°C" + "<br/>" + Math.round(fourthLow) + "°C";
+	friEmoji.innerHTML = emojiDisplay(Math.round(fourth));
+
+	const fifthDate = Object.keys(groupedData)[4];
+	const fifthData = groupedData[fifthDate];
+	const fifth = fifthData.high;
+	const fifthLow = fifthData.low;
+	const satTemp = document.querySelector(".sat > .temp");
+	const satEmoji = document.querySelector(".sat > .emoji");
+	satTemp.innerHTML =
+		Math.round(fifth) + "°C" + "<br/>" + Math.round(fifthLow) + "°C";
+	satEmoji.innerHTML = emojiDisplay(Math.round(fifth));
+
+	const sixthDate = Object.keys(groupedData)[5];
+	const sixthData = groupedData[sixthDate];
+	const sixth = sixthData.high;
+	const sixthLow = sixthData.low;
+	const sunTemp = document.querySelector(".sun > .temp");
+	const sunEmoji = document.querySelector(".sun > .emoji");
+	sunTemp.innerHTML =
+		Math.round(sixth) + "°C" + "<br/>" + Math.round(sixthLow) + "°C";
+	sunEmoji.innerHTML = emojiDisplay(Math.round(sixth));
+
+	// console.log(groupedData);
+}
+
 function searchCity(city) {
 	let apiKey = "4da81fbae0921c94a93d57a85fcd39ab";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -48,7 +170,7 @@ function searchLocation(position) {
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 	let apiForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(displayWeatherCondition);
-	axios.get(apiForecast);
+	axios.get(apiForecast).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
@@ -75,4 +197,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("New York");
+navigator.geolocation.getCurrentPosition(searchLocation);
